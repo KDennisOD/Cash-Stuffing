e49d0fd9548b41c4b36c38669425f5e31344206a
KDennisOD <github@knutti.de>
2024-09-20 23:40:22 +0200

    db new

--- models.py
+++ models.py
@@ -2,14 +2,18 @@
 from db import db
 
 class User(db.Model):
+    __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     categories = db.relationship('Category', backref='user', lazy=True)
 
 class Category(db.Model):
+    __tablename__ = 'categories'
+    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     allocated_amount = db.Column(db.Float, nullable=False)
     spent_amount = db.Column(db.Float, default=0.0)
@@ -17,7 +21,9 @@ class Category(db.Model):
     expenses = db.relationship('Expense', backref='category', lazy=True)
 
 class Expense(db.Model):
+    __tablename__ = 'expenses'
+    
     id = db.Column(db.Integer, primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     amount = db.Column(db.Float, nullable=False)
